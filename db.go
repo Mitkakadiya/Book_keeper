7b6a4f68ebb3b1f5be36a2ac0ec5d2c18e9dc5ce
Mitkakadiya <mitkakadiya127@gmail.com>
2025-05-21 15:42:29 +0530

    implement CRUD

---
+++ db.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	_ "modernc.org/sqlite" // required for driver
+)
+
+func InitializeDB() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("storage.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database")
+	}
+
+	// Auto migrate your models
+	if err := db.AutoMigrate(&User{}, &Book{}); err != nil {
+		log.Fatal("failed to migrate database: ", err)
+	}
+	return db
+}
